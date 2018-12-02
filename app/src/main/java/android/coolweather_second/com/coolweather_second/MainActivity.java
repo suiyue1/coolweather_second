b04ed99b598ac59f8e390f862b0bfa04c2d0c90a
John Doe <johndoe@example.com>
2018-12-02 18:17:18 +0800

    First commit.

---
+++ app/src/main/java/android/coolweather_second/com/coolweather_second/MainActivity.java
@@ -0,0 +1,13 @@
+package android.coolweather_second.com.coolweather_second;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
